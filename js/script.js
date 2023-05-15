0b3ac94abe0632064e7e13d9950955607621785a
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-05-15 17:11:10 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Mohamad All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohamad
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/*
+ * This function does multiploication using an old and simple method
+ */
+
+function calculate() {
+  // Define variable
+  let answer = 0
+
+  // Get user unput
+  let numberA = parseFloat(document.getElementById("numberA").value)
+  let numberB = parseFloat(document.getElementById("numberB").value)
+
+  console.log(numberA)
+  // Perform subtraction in a while loop
+  while (numberA >= numberB) {
+    // Subtract the number
+    numberA -= numberB
+    // Add the amount of times the number can be subtracted (for a final divison answer)
+    answer++
+  }
+
+  // Output answer
+  document.getElementById("answer").innerHTML = "The answer is " + answer + " R" + numberA
+}
